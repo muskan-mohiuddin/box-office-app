f1858ed8d81132a19963558dd377d4163d3702e4
muskanmohiuddin <cpie6302@gmail.com>
2024-06-15 23:40:18 -0700

    styled entire application using styled components

--- src/components/actors/ActorsGrid.jsx
+++ src/components/actors/ActorsGrid.jsx
@@ -1,7 +1,8 @@
 import ActorCard from './ActorCard';
+import { FlexGrid } from '../common/FlexGrid';
 const ActorsGrid = ({ actors }) => {
   return (
-    <div>
+    <FlexGrid>
       {actors.map(data => (
         <ActorCard
           key={data.person.id}
@@ -13,7 +14,7 @@ const ActorsGrid = ({ actors }) => {
           image={data.person.image ? data.person.image.medium : '/imggg.png'}
         />
       ))}
-    </div>
+    </FlexGrid>
   );
 };
 export default ActorsGrid;
--- src/pages/Starred.jsx
+++ src/pages/Starred.jsx
@@ -2,31 +2,30 @@ import { useQuery } from '@tanstack/react-query';
 import { getShowIds } from '../api/tvmaze';
 import { useStarredShows } from '../lib/useStarredShows';
 import ShowGrid from '../components/shows/ShowGrid';
-
+import { TextCenter } from '../components/common/TextCenter';
 const Starred = () => {
   const [starredShowsIds] = useStarredShows();
   const { data: starredShows, error: starredShowsError } = useQuery({
     queryKey: ['starred', starredShowsIds],
-    queryFn: async () =>
+    queryFn: () =>
       getShowIds(starredShowsIds).then(result =>
         result.map(show => ({ show }))
       ),
 
     refetchOnWindowFocus: false,
   });
-  console.log({ starredShows });
 
   if (starredShows?.length === 0) {
-    return <div>No shows were starred</div>;
+    return <TextCenter>No shows were starred</TextCenter>;
   }
 
   if (starredShows?.length > 0) {
     return <ShowGrid shows={starredShows} />;
   }
   if (starredShowsError) {
-    return <div>Error occured: {starredShowsError.message}</div>;
+    return <TextCenter>Error occured: {starredShowsError.message}</TextCenter>;
   }
 
-  return <div>Shows are loading</div>;
+  return <TextCenter>Shows are loading</TextCenter>;
 };
 export default Starred;
